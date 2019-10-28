d562da3948296c51ecb1a11254c37e67ffbeb212
Felipe Bergamin <felipebergamin6@gmail.com>
2019-10-28 16:28:10 -0300

    simple test

---
+++ src/components/TechList/index.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import {View, TextInput, TouchableOpacity, Text, FlatList} from 'react-native';
+
+// import { Container } from './styles';
+
+export default function TechList() {
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={techs}
+        keyExtractor={tech => tech}
+        renderItem={({item}) => <Text>{item}</Text>}
+      />
+
+      <TextInput
+        value={newTech}
+        onChangeText={setNewTech}
+        testID="tech-input"
+      />
+
+      <TouchableOpacity onPress={handleAdd}>
+        <Text>Adicionar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
